c7d83a57def8666d27f87edc764e6bad570f7b56
khanjasir90 <khanmohdjasir@gmail.com>
2023-01-09 15:35:12 +0530

    added async fetch function
    Signed-off-by: khanjasir90 <khanmohdjasir@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const { Router } = require('express');
 const PORT = 3000;
+
+async function fetchData(id) {
+    const URL = `https://dummy.restapiexample.com/api/v1/employee/${id}`;
+    const result = await axios.get(URL);
+    console.log('Request Send');
+    console.log(result);
+    return res.status(200).json({
+        data: result.data
+    });
+}
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
